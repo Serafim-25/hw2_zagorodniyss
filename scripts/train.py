df9601a1925e917c5407961d77e5e7a50060d8fa
Серафим <zagorodniy.serafim@mail.ru>
2025-01-31 12:30:36 +0300

    Added file

--- scripts/train.py
+++ scripts/train.py
@@ -16,10 +16,10 @@ def main():
     # Создаем парсер для аргументов командной строки
     parser = argparse.ArgumentParser(
         description="Train a logistic regression model")
-    
+
     # Добавляем аргумент для указания типа модели
     parser.add_argument("--model-type", type=str, default="model_1", help="Type of model to train")
-    
+
     # Разбираем аргументы командной строки
     args = parser.parse_args()
 
@@ -34,7 +34,7 @@ def main():
     # Разделяем данные на обучающую и тестовую выборки
     X_train, X_test, y_train, y_test = train_test_split(
         X, y, test_size=0.2, random_state=42)
-    
+
     # Устанавливаем параметры для модели логистической
     # регрессии в зависимости от типа модели
     if args.model_type == "model_1":
@@ -48,17 +48,17 @@ def main():
     task = Task.current_task()
     # Если задача не найдена, используем "local" как имя задачи
     task_name = task.name if task else "local"
-    
+
     # Запускаем эксперимент в MLflow
     with mlflow.start_run(run_name=f"{task_name}-{args.model_type}"):
         # Логируем параметры модели
         mlflow.log_param("C", C)
         mlflow.log_param("solver", solver)
-         
+
         # Инициализируем модель логистической
         # регрессии с выбранными параметрами
         model = LogisticRegression(C=C, solver=solver)
-         
+
         # Обучаем модель
         model.fit(X_train, y_train)
 
@@ -67,15 +67,16 @@ def main():
 
         # Рассчитываем точность модели
         accuracy = accuracy_score(y_test, y_pred)
-         
+
         # Логируем метрику точности в MLflow
         mlflow.log_metric("accuracy", accuracy)
          
         # Логируем метрику точности в задаче ClearML
         if task:
-            task.logger.report_scalar(title="Accuracy", series="Accuracy", value=accuracy)
-    
-         # Логируем модель в MLflow
+            task.logger.report_scalar(
+                title="Accuracy", series="Accuracy", value=accuracy)
+
+        # Логируем модель в MLflow
         mlflow.sklearn.log_model(model, artifact_path="logistic_regression")
 
     # Выводим финальное сообщение о завершении обучения
