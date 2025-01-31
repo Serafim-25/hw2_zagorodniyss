a0af977c4c029937f58ea85d49bc1c4bd00c52d9
Серафим <zagorodniy.serafim@mail.ru>
2025-01-31 11:03:07 +0300

    Added file

--- scripts/download.py
+++ scripts/download.py
@@ -1,6 +1,9 @@
 import os  # Импортируем модуль для работы с операционной системой
 import pandas as pd  # Импортируем pandas для работы с данными в формате DataFrame
-from sklearn.datasets import load_iris  # Импортируем функцию для загрузки набора данных iris_data из sklearn
+from sklearn.datasets import (
+    load_iris,
+)  # Импортируем функцию для загрузки набора данных iris_data из sklearn
+
 
 def download_data(output_path):
     # Функция для загрузки набора данных iris_data и сохранения его в формате CSV
@@ -10,16 +13,23 @@ def download_data(output_path):
     iris_data = load_iris()  # Загрузка набора данных iris_data
 
     # Преобразуем данные в pandas DataFrame для удобной работы
-    iris_df = pd.DataFrame(data=iris_data.data, columns=iris_data.feature_names)  # Создаем DataFrame с данными признаков (features)
+    iris_df = pd.DataFrame(
+        data=iris_data.data, columns=iris_data.feature_names
+    )  # Создаем DataFrame с данными признаков (features)
 
     # Добавляем столбец 'species' с целевыми метками (классами)
-    iris_df['species'] = iris_data.target  # Добавляем столбец 'species' с целевыми метками, представляющими виды
+    iris_df["species"] = (
+        iris_data.target
+    )  # Добавляем столбец 'species' с целевыми метками, представляющими виды
 
     # Сохраняем DataFrame в CSV файл по указанному пути
     iris_df.to_csv(output_path, index=False)  # Сохраняем в CSV, без индекса
 
     # Выводим сообщение, что данные успешно загружены
-    print('Данные были загружены')  # Подтверждение загрузки данных
+    print("Данные были загружены")  # Подтверждение загрузки данных
+
 
 # Загружаем и сохраняем данные в файл 'data/iris_dataset.csv'
-download_data('data/iris_dataset.csv')  # Вызываем функцию для загрузки данных и их сохранения
+download_data(
+    "data/iris_dataset.csv"
+)  # Вызываем функцию для загрузки данных и их сохранения
--- scripts/pipeline.py
+++ scripts/pipeline.py
@@ -15,12 +15,14 @@ def dvc_pull():
     # Выполняем команду "dvc pull", которая обновляет локальную копию данных и моделей
     subprocess.run(["dvc", "pull"], check=True)
 
+
 # Декоратор PipelineDecorator для компонента download_data, который не использует кеширование
 # и выполняется в очереди "default". Этот компонент запускает скрипт для загрузки данных.
 @PipelineDecorator.component(cache=False, execution_queue="default")
 def download_data():
     # Запускаем Python-скрипт "download.py" для загрузки необходимых данных
     subprocess.run(["python", "scripts/download.py"], check=True)
+
 
 # Декоратор PipelineDecorator для компонента process_data, который не использует кеширование
 # и выполняется в очереди "default". Этот компонент запускает скрипт для обработки данных.
@@ -37,24 +39,30 @@ def train_model_1():
     try:
         # Получаем текущую задачу с помощью метода Task.current_task()
         current_task = Task.current_task()
-        
+
         # Если задача существует, продолжаем выполнение
         if current_task:
             # Извлекаем ID текущей задачи и выводим его в консоль для логирования
             task_id = current_task.id
             print(f"Идентификатор текущей задачи в train_model_1: {task_id}")
-            
+
             # Инициализируем новую задачу в проекте "my_mlops_project" для обучения модели
-            new_task = Task.init(project_name="my_mlops_project", task_name="train_with_clearml_1")
-            
+            new_task = Task.init(
+                project_name="my_mlops_project", task_name="train_with_clearml_1"
+            )
+
             # Запускаем задачу для выполнения удаленно на очереди 'default'
-            new_task.execute_remotely(queue_name='default')  # Запуск задачи на удаленной машине/агенте
+            new_task.execute_remotely(
+                queue_name="default"
+            )  # Запуск задачи на удаленной машине/агенте
 
             # Логируем начало обучения модели
             print("Начинато обучение модели 1...")
-            
+
             # Используем subprocess для выполнения скрипта "train.py", передавая параметр "--model-type" для выбора модели
-            subprocess.run(["python", "scripts/train.py", "--model-type", "model_1"], check=True)
+            subprocess.run(
+                ["python", "scripts/train.py", "--model-type", "model_1"], check=True
+            )
 
         else:
             # Если текущая задача не существует, выводим ошибку и выбрасываем исключение
@@ -73,24 +81,30 @@ def train_model_2():
     try:
         # Получаем текущую задачу с помощью метода Task.current_task()
         current_task = Task.current_task()
-        
+
         # Если задача существует, продолжаем выполнение
         if current_task:
             # Извлекаем ID текущей задачи и выводим его в консоль для логирования
             task_id = current_task.id
             print(f"Идентификатор текущей задачи в train_model_2: {task_id}")
-            
+
             # Инициализируем новую задачу в проекте "my_mlops_project" для обучения второй модели
-            new_task = Task.init(project_name="my_mlops_project", task_name="train_with_clearml_2")
-            
+            new_task = Task.init(
+                project_name="my_mlops_project", task_name="train_with_clearml_2"
+            )
+
             # Запускаем задачу для выполнения удаленно на очереди 'default'
-            new_task.execute_remotely(queue_name='default')  # Запуск задачи на удаленной машине/агенте
+            new_task.execute_remotely(
+                queue_name="default"
+            )  # Запуск задачи на удаленной машине/агенте
 
             # Логируем начало обучения второй модели
             print("Начинато обучение модели 2...")
-            
+
             # Используем subprocess для выполнения скрипта "train.py", передавая параметр "--model-type" для выбора второй модели
-            subprocess.run(["python", "scripts/train.py", "--model-type", "model_2"], check=True)
+            subprocess.run(
+                ["python", "scripts/train.py", "--model-type", "model_2"], check=True
+            )
 
         else:
             # Если текущая задача не существует, выводим ошибку и выбрасываем исключение
@@ -101,12 +115,14 @@ def train_model_2():
         print(f"Ошибка в компоненте train_model_2: {e}")
         raise  # Пробрасываем исключение дальше
 
+
 # Декоратор PipelineDecorator для компонента dvc_repro, который выполняет команду DVC "repro".
 # Эта команда запускает воспроизведение состояния данных в DVC, восстанавливая нужные версии данных и зависимостей.
 @PipelineDecorator.component(cache=False, execution_queue="default")
 def dvc_repro():
     # Выполняем команду "dvc repro" для воспроизведения данных и состояний в DVC
     subprocess.run(["dvc", "repro"], check=True)
+
 
 # Декоратор PipelineDecorator для компонента dvc_push, который выполняет команду DVC "push".
 # Эта команда отправляет изменения в репозиторий DVC, обновляя удаленные хранилища данными.
@@ -115,26 +131,26 @@ def dvc_push():
     # Выполняем команду "dvc push" для отправки изменений в DVC хранилище
     subprocess.run(["dvc", "push"], check=True)
 
+
 # Декоратор PipelineDecorator для определения пайплайна 'mlops_pipeline'.
 # Указываем название пайплайна, проект и версию, чтобы управлять различными этапами работы с ClearML.
 @PipelineDecorator.pipeline(
-    name='mlops_pipeline',
-    project='my_mlops_project',
-    version='0.1'
+    name="mlops_pipeline", project="my_mlops_project", version="0.1"
 )
 def mlops_pipeline_logic():
-  # Шаги пайплайна, который включает в себя несколько этапов, включая работу с DVC и обработку данных.
-  dvc_pull()      # Загружаем данные с помощью DVC (pull).
-  download_data()  # Скачиваем необходимые данные с использованием скрипта download.py.
-  process_data()   # Обрабатываем данные с помощью скрипта process_data.py.
-  train_model_1()  # Обучаем первую модель.
-  train_model_2()  # Обучаем вторую модель.
-  dvc_repro()      # Воспроизводим данные и зависимости с помощью DVC (repro).
-  dvc_push()       # Отправляем обновленные данные в DVC хранилище (push).
+    # Шаги пайплайна, который включает в себя несколько этапов, включая работу с DVC и обработку данных.
+    dvc_pull()  # Загружаем данные с помощью DVC (pull).
+    download_data()  # Скачиваем необходимые данные с использованием скрипта download.py.
+    process_data()  # Обрабатываем данные с помощью скрипта process_data.py.
+    train_model_1()  # Обучаем первую модель.
+    train_model_2()  # Обучаем вторую модель.
+    dvc_repro()  # Воспроизводим данные и зависимости с помощью DVC (repro).
+    dvc_push()  # Отправляем обновленные данные в DVC хранилище (push).
+
 
 # Основной блок для выполнения пайплайна на локальной машине, полезно для отладки.
 # Для масштабирования, нужно будет закомментировать следующую строку и настроить очередь 'services' на ClearML.
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Запуск пайплайна локально, используем для тестирования на текущей машине
     # Для масштабирования можно закомментировать эту строку и использовать очередь 'services' с агентом ClearML
     PipelineDecorator.run_locally()  # Запуск пайплайна локально для отладки
